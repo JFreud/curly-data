9523305cab7a9b182a2a0a49674ab9321be3060c
JFreud <jrm.frdnbrg@gmail.com>
2018-02-25 15:00:12 -0500

    new stuffs

---
+++ mongodata.py
@@ -0,0 +1,64 @@
+# Name of dataset: American Movies Scraped from Wikipedia
+# Description: Name and related info about American Movies that are listed on Wikipedia
+# Hyperlink: https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json
+# Import mechanism:
+
+import pymongo, requests, json
+
+connection = pymongo.MongoClient("homer.stuy.edu")
+db = connection["freudenbergJ-zorinM"]
+movies = db["movies"]
+
+r = requests.get("https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json")
+data = r.json()
+
+for doc in data:
+    movies.insert_one(doc)
+
+
+def get_year(year):
+    year_cursor = movies.find({"year" : year})
+    L = list()
+    for movie in year_cursor:
+        L.append(movie)
+    return L
+
+def get_director(director):
+    direct_cursor = movies.find({"director" : director})
+    L = list()
+    for movie in direct_cursor:
+        L.append(movie)
+    return L
+
+def director_genre(director, genre):
+    cursor = movies.find({"director" : director, "genre" : genre})
+    L = list()
+    for movie in cursor:
+        L.append(movie)
+    return L
+
+def director_year(director, year):
+    cursor = movies.find({"director" : director, "year" : year})
+    L = list()
+    for movie in cursor:
+        L.append(movie)
+    return L
+
+def get_movie(title):
+    cursor = movies.find({"title" : title})
+    L = list()
+    for movie in cursor:
+        L.append(movie)
+    return L
+
+if __name__ == '__main__':
+    print "Movies from 2016:"
+    print get_year(2016)
+    print "Movies by Christopher Nolan"
+    print get_director("Christopher Nolan")
+    print "Science Fiction Movies by Christopher Nolan"
+    print director_genre("Christopher Nolan", "Science Fiction")
+    print "2014 movies by Christopher Nolan"
+    print director_year("Christopher Nolan", "2014")
+    print "Info about Interstellar"
+    print get_movie("Interstellar")
